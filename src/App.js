b6b61ebf09004df5e42c770accb89c84e7d293d5
Sabir Ali <hakrosabir@gmail.com>
2022-03-14 14:34:37 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1> In the Name of Allah </h1>
+      <h2> I am Starting again to develop the react apps </h2>
+    </div>
+  );
+}
